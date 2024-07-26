3f14958abf43ffeb67c5f53833873891f8969934
rashadmin <abdul.a.rasheed2021@gmail.com>
2024-07-26 09:56:41 +0100

    added links

--- src/components/CvButton.js
+++ src/components/CvButton.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
 import { FaArrowAltCircleDown } from 'react-icons/fa';
 function CvButton({className}) {
   return (
+    <Nav.Link href="https://www.google.com" target='_blank'>
     <Button variant="info" className={`${className}-name`}><span className={`${className}-text`}> Download CV </span><FaArrowAltCircleDown className={`${className}-icon`} /></Button>
+    </Nav.Link>
   )
 }
 
--- src/components/socialIcons.js
+++ src/components/socialIcons.js
@@ -1,18 +1,17 @@
 import React from 'react'
-// import Nav from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import {FaGithub,FaWhatsapp,FaLinkedin} from "react-icons/fa"
 import { FaXTwitter } from "react-icons/fa6";
 function SocialIcons({className}) {
   return (
-    <div className={className}>
-              
-             <a href="www.google.com" target='_blank'> <FaGithub className='social github'></FaGithub></a>
-            <a href="www.google.com" target='_blank'> <FaLinkedin className='social linkedin'></FaLinkedin></a>
-            <a href="www.google.com" target='_blank'>  <FaXTwitter className='social twitter'></FaXTwitter></a>
-            <a href="www.google.com" target='_blank'>  <FaWhatsapp className='social whatsapp'></FaWhatsapp></a>
+    <div className={className}> 
+            <Nav.Link className='social github' href="https://github.com/rashadmin" target='_blank'><FaGithub/></Nav.Link>
+            <Nav.Link className='social twitter' href="https://www.linkedin.com/in/abdulrasheed-ameen-27829822a/" target='_blank'><FaLinkedin/></Nav.Link>
+            <Nav.Link className='social twitter' href="https://www.google.com" target='_blank'><FaXTwitter/></Nav.Link>
+            <Nav.Link className='social whatsapp' href="https://wa.link/auwl1b" target='_blank'><FaWhatsapp/></Nav.Link>
     </div>
   )
 }
 
-export default SocialIcons
+export default SocialIcons  
