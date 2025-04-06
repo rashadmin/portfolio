81c4469358049d50579cc2d5e0763b8f590666eb
Ameen Abdulrasheed <abdul.a.rasheed2021@gmail.com>
2025-04-06 07:53:46 -0700

    Update AboutmeDesc.js

--- src/components/AboutmeDesc.js
+++ src/components/AboutmeDesc.js
@@ -3,7 +3,7 @@ import React from 'react'
 function AboutmeDesc() {
   return (
          <div className='text-content'>
-          <p> I’m Ameen Abdulrasheed (yes, the surname has its own charm 😉). Split between Lagos and Ilorin ,Nigeria , I’m here to build meaningful connections as life’s "playtime" evolves into deeper chapters.</p>
+          <p> Hi everyone! I’m Ameen Abdulrasheed (yes, the surname has its own charm 😉). Split between Lagos and Ilorin ,Nigeria , I’m here to build meaningful connections as life’s "playtime" evolves into deeper chapters.</p>
           <p>I turn data into stories and solutions as a Data Scientist & Engineer (in view). By day, I code in Python/SQL, analyze trends, and engineer software (Flask, React, JS). By night? I’m probably geeking out over anime, tweaking my <a href='https://py-blog-two.vercel.app'>blog</a>, or taking midnight strolls under a cozy weather</p>
           </div>
 
