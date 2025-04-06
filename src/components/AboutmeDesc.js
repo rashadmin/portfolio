55a3518131667419166f687c073e3eee808dd520
Ameen Abdulrasheed <abdul.a.rasheed2021@gmail.com>
2025-04-06 08:13:39 -0700

    Update AboutmeDesc.js

--- src/components/AboutmeDesc.js
+++ src/components/AboutmeDesc.js
@@ -5,9 +5,8 @@ function AboutmeDesc() {
          <div className='text-content'>
           <p> Hi everyone! I’m Ameen Abdulrasheed (yes, the surname has its own charm 😉). Split between Lagos and Ilorin ,Nigeria , I’m here to build meaningful connections as life’s "playtime" evolves into deeper chapters.</p>
           <p> I turn data into stories and solutions as a Data Scientist & Engineer (in view). By day, I code in Python/SQL, analyze trends, and engineer software (Flask, React, JS). By night? I’m probably geeking out over anime, tweaking my 
-              <a href='https://py-blog-two.vercel.app' style="background-color: #0d6efd; color: white; padding: 2px 6px; border-radius: 6px; text-decoration: none; transition: all 0.3s ease; box-shadow: 0 2px 6px rgba(13, 110, 253, 0.4);">
-    blog </a>, or taking midnight strolls under a cozy weather
-</p>
+              <a href='https://py-blog-two.vercel.app' style="background-color: #0d6efd; color: white; padding: 2px 6px; border-radius: 6px; text-decoration: none; transition: all 0.3s ease; box-shadow: 0 2px 6px rgba(13, 110, 253, 0.4);">blog </a>, or taking midnight strolls under a cozy weather
+          </p>
      
     // <p>I turn data into stories and solutions as a Data Scientist & Engineer (in view). By day, I code in Python/SQL, analyze trends, and engineer software (Flask, React, JS). By night? I’m probably geeking out over anime, tweaking my <a href='https://py-blog-two.vercel.app'>blog</a>, or taking midnight strolls under a cozy weather</p>
           </div>
